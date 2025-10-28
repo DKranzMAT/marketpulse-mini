a2d264c017454e51caa7cc89ba52aad0dc052058
Dave <krnzdvd@yahoo.com>
2025-10-28 15:51:28 -0500

    add Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/marketpulse-mini/', // 👈 important for GitHub Pages
+  base: '/marketpulse-mini/',  // 👈 absolutely required for GitHub Pages
 })
